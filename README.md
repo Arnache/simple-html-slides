c18c065f61c2800e971f46a59165c37a018b71b8
Arnaud Chéritat <Arnache@users.noreply.github.com>
2026-04-21 20:11:22 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # simple-html-slides
-A minimal slides system for HTML presentations
+
+A minimal slides system for HTML presentations.
+
+Right now it is only compatible with desktop and laptop, *not* touch devices (mobile phones, tablets, ...).
+
+The code is in `files/` and consists in the companion file `pages.js`to be put in the folder containing the HTML presentation files. Each slide is a separate html file [1] that must each load (near the end) the pages.js as a script.
+
+The file pages.js starts with an `Array` containing the (ordered) list of your slides: just modify it. It looks like this:
+
+```js
+let pages = [
+ "index.html",
+
+"page-1.html",
+ "page-2.html",
+
+"hex-1.html",
+ "hex-2.html",
+
+"end.html",
+];
+```
+
+On the browser, you switch slides with PageUp and PageDown keyboard key.
+
+Items can be hidden, and revealed by using the left and right arrow keys (that changes slides if there is nothing more to reveal/hide). For this, put the class `reveal` in the tag of the concerned element(s).
+
+Folder `example/` contains an example, just look. It contains in particular a simple example of css style file called `style.css`. The design of this style file can be improved (right now it uses a mix of absolute and relative size units).
+
+A working example (with a former version: arrows don't transition between slides, only page up/down) can be found online at: https://www.math.univ-toulouse.fr/~cheritat/Exposes/IHP-2026-3/ if that link is still active. 
+
+[1] This is not optimal in terms of pre-loading things but for normal simple presentations, this should work.
---
+++ desktop/files/pages.js
@@ -0,0 +1,91 @@
+let pages = [
+  "index.html",
+  
+  "page-1.html",
+  "page-2.html",
+  
+  "hex-1.html",
+  "hex-2.html",
+
+  "end.html",
+];
+
+
+/* slides */
+
+function testAndGo(newPage) {
+ var req = new XMLHttpRequest();
+ req.onreadystatechange = function() {
+   if(this.readyState == 4) {
+     if(this.status==200) {
+       window.location.href = newPage;
+     }
+   }
+ }
+ req.open("HEAD", newPage);
+ req.send();
+}
+
+let href = window.location.href;
+let pageName = href.substring(href.lastIndexOf('/') + 1);
+
+let pageIndex = pages.indexOf(pageName);
+if(pageIndex<0) pageIndex = 0;
+
+function forward() {
+  if(pageIndex < pages.length-1) testAndGo(pages[pageIndex+1]);
+}
+
+function backward() {
+  if(pageIndex > 0) testAndGo(pages[pageIndex-1]);
+}
+
+document.addEventListener('keydown',  e => {
+  if(e.key=="PageDown") {
+    forward();
+  }
+  else if(e.key=="PageUp") {
+    backward();
+  }
+  else if(e.key=="Home" && pageIndex > 0 ) {
+    testAndGo(pages[0]);
+  }
+  else if(e.key=="End" && pageIndex < pages.length-1 ) {
+    testAndGo(pages[pages.length-1]);
+  }
+});
+
+/* reveal */
+
+let revList = document.querySelectorAll(".reveal");
+
+// revList.forEach( el => el.style.visibility = "hidden" ); // done by the stylesheet
+
+let index = 0;
+
+function next() {
+  if(index<revList.length) {
+    revList[index].style.visibility = "visible";
+    index++;
+  }
+  else {
+    forward();
+  }  
+}
+
+function prev() {
+  if(index>0) {
+    index--;
+    revList[index].style.visibility = "hidden";
+  }
+  else {
+    backward();
+  }
+}
+
+document.addEventListener('keydown',  e => {
+  if(e.key==" " || e.key=="ArrowRight")
+    next();
+  else if(e.key=="ArrowLeft")
+    prev();
+});
